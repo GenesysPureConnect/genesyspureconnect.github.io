029691ed6bae6f69753e466e85a00fabcc414605
stormojm <stormojm@gmail.com>
2018-05-16 09:45:25 +0200

    Changing QA link to community.genesys.com

--- js/tabs.js
+++ js/tabs.js
@@ -9,7 +9,7 @@ new TabSpec('home', 'https://developer.inin.com/Pages/default.aspx'),
 new TabSpec('Blog', 'https://developer.inin.com/blog/pages/default.aspx'),
 new TabSpec('Documentation', 'https://developer.inin.com/documentation/pages/default.aspx'),
 new TabSpec('Open Source', ''),
-new TabSpec('Q&A', 'http://community.inin.com/forums/forumdisplay.php?126-Developer-Forums'),
+new TabSpec('Q&A', 'http://community.genesys.com'),
 //new TabSpec('applications', 'tab-content-api-applications', 'Applications'),
 
 //new TabSpec('utilities', 'tab-content-utilities', 'Utilities'),
